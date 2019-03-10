0bb9fd3e5a6c81e2c15368daea7d570ea414e9ac
sinmetal <metal.tie@gmail.com>
2019-03-11 00:20:40 +0900

    NumChan=1

--- spanner.go
+++ spanner.go
@@ -13,6 +13,7 @@ import (
 
 func createClient(ctx context.Context, db string, o ...option.ClientOption) (*spanner.Client, error) {
 	config := spanner.ClientConfig{
+		NumChannels: 1,
 		SessionPoolConfig: spanner.SessionPoolConfig{
 			MinOpened: 50,
 		},
