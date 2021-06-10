09cb14bbaa4302cc6874b1f99ae63c87d9beffd6
sinmetal <sinmetal@sinmetalcraft.jp>
2021-06-10 20:56:21 +0900

    intにNULLが入ってるとGo側がめんどくさいので、NOT NULLにした

--- ddl/score.sql
+++ ddl/score.sql
@@ -8,9 +8,9 @@ CREATE TABLE ScoreUser (
 CREATE TABLE Score (
   Id STRING(MAX) NOT NULL,
   ClassRank INT64 AS (IF(Score > 1000000000, 6, IF(Score > 100000000, 5, IF(Score > 10000000, 4, IF(Score > 1000000, 3, IF(Score > 100000, 2, IF(Score > 10000, 1, 0))))))) STORED,
-  CircleID STRING(MAX),
-  Score INT64,
-  MaxScore INT64,
+  CircleID STRING(MAX) NOT NULL,
+  Score INT64 NOT NULL,
+  MaxScore INT64 NOT NULL,
   CommitedAt TIMESTAMP NOT NULL OPTIONS (
     allow_commit_timestamp = true
   ),
